3eb448e1ccaf6e4a8e2fb801a47cff05414acbb3
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-08 12:36:28 +0100

    Create Mastodon_002.sh

---
+++ Mastodon_002.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
+cd ~/.rbenv && src/configure && make -C src
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+exec bash
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+RUBY_CONFIGURE_OPTS=--with-jemalloc rbenv install 3.0.4
+
+rbenv global 3.0.4
+
+gem install bundler --no-document
