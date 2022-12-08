845df3a6c6ea1615dfd2089930e3cc522ebe081e
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-08 13:29:26 +0100

    Update Mastodon_004.sh

--- Mastodon_004.sh
+++ Mastodon_004.sh
@@ -15,3 +15,5 @@ mkdir cert
 cd cert
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout example.key -out example.crt -subj "/CN=example.com" -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1"
+
+cd
