68651ed81cca2d1d07536c7284b61e66b663e932
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-08 12:46:32 +0100

    Create Mastodon_004.sh

---
+++ Mastodon_004.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/mastodon/mastodon.git /home/mastodon/live && cd /home/mastodon/live
+
+git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
+
+bundle config deployment 'true'
+
+bundle config without 'development test'
+
+bundle install -j$(getconf _NPROCESSORS_ONLN)
+
+yarn install --pure-lockfile
